25b5421a4385f9da9806c0c7050560675aa17cd4
IMPraveenRaj <impraveen13@gmail.com>
2024-11-16 19:52:02 +0100

    yaml changes

--- my-first-yml/src/main/java/com/springheaven/yaml/App.java
+++ my-first-yml/src/main/java/com/springheaven/yaml/App.java
@@ -1,13 +1,36 @@
 package com.springheaven.yaml;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	public void readYmlData() throws IOException {
+		Yaml yaml= new Yaml();
+		
+		InputStream resourceAsStream = this.getClass().getClassLoader().
+				getResourceAsStream("sample.yml");
+		Map<String,Object> map= yaml.load(resourceAsStream);
+		Object object = yaml.load(resourceAsStream);
+		Map<String,Object> map2= yaml.load(resourceAsStream);
+		/* var map= yaml.load(resourceAsStream); */
+		System.out.println(map);;
+		
+	}
+	
+	
+	
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        new App().readYmlData();
     }
 }
